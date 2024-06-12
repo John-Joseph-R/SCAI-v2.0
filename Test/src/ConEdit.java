ef44454790aafa10e72e491a8e326f16f0d9c80b
John Joseph R <73868284+John-Joseph-R@users.noreply.github.com>
2024-06-12 23:02:08 +0530

    ConEdit.java

--- Test/src/ConEdit.java
+++ Test/src/ConEdit.java
@@ -9,7 +9,7 @@ public class ConEdit extends JFrame implements ActionListener {
     // JDBC URL, username, and password of MySQL server
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/scai";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "chrisjohn98";
+    private static final String PASSWORD = "ENTER PASSWORD HERE";
 
     private JTextField userIDField, conNameField, phNoField;
     private JButton insertButton, viewButton;
